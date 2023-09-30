8f426719e158b448d74642cf8080dc06a06e01a8
蔡溢漳 <1031282751@qq.com>
2023-10-01 01:47:32 +0800

    删除

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_ROOT: '"https://mock.apifox.cn/m1/3355434-0-default"'
-})
