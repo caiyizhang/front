7e964b7cde741847af4ded8ca5a160171a9186c5
蔡溢漳 <1031282751@qq.com>
2023-10-01 01:49:15 +0800

    重新添加

---
+++ src/components/Weixin.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div id="wx_login_container" style="height: 300px"></div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {}
+    },
+    mounted() {
+        this.get_wx_qrcode();
+    },
+    methods: {
+        get_wx_qrcode() {
+            let wx_login = new WxLogin({
+                id: "wx_login_container",
+                // appid: "wxf200e30fb49fdb0e",
+                  appid: "wx53cf447461989356",
+                scope: "snsapi_login",
+                redirect_uri: encodeURIComponent("http://www.template.matrix-studio.top/#/login"),
+                state: Math.ceil(Math.random() * 100),
+                style: "black",
+                href: "",
+                self_redirect: false,
+            });
+        }
+    }
+}
+</script>
